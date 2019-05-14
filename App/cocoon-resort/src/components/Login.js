e49281e320df59524c0bf54490fc92f025c44044
KVNbog <kevin.boghossian@gmail.com>
2019-05-14 06:20:53 +0300

    feat: ya 3adra

---
+++ App/cocoon-resort/src/components/Login.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Form, Modal, Button, Icon, Message } from 'semantic-ui-react';
+import loginClass from '../classes/auth';
+
+class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        errorMessage: ''
+    };
+
+    render() {
+        return (
+            <Modal
+                open={this.props.isLoginOpen}
+                onClose={this.loginClose}
+                size='small'
+                closeOnDimmerClick={false}
+                >
+                <Modal.Header>
+                    <Icon name='sign-in' /> Log In
+                </Modal.Header>
+
+                <Modal.Content>
+                    <Form error={!!this.state.errorMessage} onSubmit={this.submit}>
+                        <Form.Input
+                            label='Email'
+                            placeholder='Email'
+                            name='email'
+                            value={this.state.email}
+                            onChange={event => this.setState({ email: event.target.value })}
+                            />
+
+                        <Form.Input
+                            label='Password'
+                            placeholder='Password'
+                            name='password'
+                            value={this.state.password}
+                            onChange={event => this.setState({ password: event.target.value })}
+                            />
+
+                        <Message error header="Oops!" content={this.state.errorMessage} />
+                        <Form.Button
+                            positive
+                            loading={this.state.loading}
+                            content='Log In' floated='right'/>
+                        <Button secondary onClick={this.loginClose} content='Cancel'/>
+                    </Form>
+                </Modal.Content>
+            </Modal>
+        );
+    }
+
+    submit = async () => {
+        this.setState({ errorMessage: '' });
+        const { email, password } = this.state;
+        if (email === '' || password === '') {
+            this.setState({ errorMessage: 'Some Fields are Missing!' });
+        } else {
+            const login = new loginClass();
+            const result = await login.authUser(email, password);
+
+            if (result !== 404) {
+                this.props.handleLogin(result[0].email, result[0].name);
+            } else {
+                this.setState({ errorMessage: 'Invalid Username/Password' });
+            }
+        }
+    }
+
+    loginClose = () => {
+        this.setState({ email: '', password: '', errorMessage: '' });
+        this.props.loginClose();
+    }
+}
+
+export default Login;
---
+++ App/cocoon-resort/src/components/NavigationBar.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Menu, MenuItem, Button, Dropdown, Form, Icon } from 'semantic-ui-react';
+import Login from '../components/Login';
+import loginClass from '../classes/auth';
+const cookie = require('../cookie');
+
+class NavigationBar extends Component {
+    state = {
+        name: '',
+        search: '',
+        isLoginOpen: false
+    };
+
+    render() {
+        return (
+            <Menu fixed="top" color="green">
+                <MenuItem>
+                    <a href="/home">LOGO</a>
+                </MenuItem>
+
+                <MenuItem style={{marginLeft: '300px'}}>
+                    <Form>
+                        <Form.Input
+                            style={{width: '400px'}}
+                            name='search'
+                            value={this.state.search}
+                            onChange={event => this.setState({ search: event.target.value })}
+                            icon='search'
+                            placeholder='Search Resorts'
+                            />
+                    </Form>
+                </MenuItem>
+
+                <Menu.Menu position = 'right'>
+                    {this.renderMenu()}
+                </Menu.Menu>
+            </Menu>
+        );
+    }
+
+    async componentDidMount() {
+        let name = cookie.getCookie('name');
+        if (name) {
+            this.setState({ name });
+        }
+    }
+
+    renderMenu = () => {
+        if (this.state.name) {
+            return (
+                <Menu.Menu position = 'right'>
+                    <Dropdown text={`Hello ${this.state.name}`} className='item'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>
+                                <Icon name='settings' /><a href='/settings' className='black'>Settings</a>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                <Icon name='map signs'/><a href='/reservations'>Reservations</a>
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={this.logout}>
+                                <Icon name='log out' />Logout
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+            )
+        } else {
+            return (
+                <Menu.Menu position = 'right'>
+
+                    <MenuItem>
+                        <Login isLoginOpen={this.state.isLoginOpen} loginClose={this.loginClose} handleLogin={this.handleLogin}/>
+                        <Button icon='sign-in' secondary inverted onClick={this.onClick} content='Log In'></Button>
+                    </MenuItem>
+
+                    <MenuItem>
+                        <Icon name='signup' /><a href='/signup'>Sign Up</a>
+                    </MenuItem>
+                </Menu.Menu>
+            );
+        }
+    }
+    onClick = (event) => {
+        event.preventDefault();
+        event.target.blur();
+        this.setState({ isLoginOpen: true });
+    }
+
+    handleLogin = (email, name) => {
+        cookie.setCookie('email', email, 100);
+        cookie.setCookie('name', name, 100);
+        this.loginClose();
+        this.setState({ name });
+    }
+
+    loginClose = () => {
+        this.setState({ isLoginOpen: false });
+    }
+
+    logout = async () => {
+        const login = new loginClass();
+        const result = await login.logoutUser();
+
+        cookie.deleteCookie();
+        window.location.href = `http://localhost:3000/home`;
+    }
+}
+
+export default NavigationBar;
