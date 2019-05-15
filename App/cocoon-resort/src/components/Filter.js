d19dcb10e5431675300be3287cc3e9611cacc806
rbtkay <robert.g.khayat@gmail.com>
2019-05-16 01:12:17 +0300

    feat: filter component done

--- App/cocoon-resort/src/components/Filter.js
+++ App/cocoon-resort/src/components/Filter.js
@@ -1,79 +1,153 @@
 import React, { Component } from 'react';
-import { Menu, Dropdown, Select, Grid } from 'semantic-ui-react';
+import { Menu, Dropdown, Select, Grid, Form, Button } from 'semantic-ui-react';
 import { DateInput } from 'semantic-ui-calendar-react';
+import NumericInput from 'react-numeric-input';
+
 import Resort from '../classes/resort';
 
 class Filter extends Component {
 
-    state = {
-        locationOptions: [],
-        location: '',
-        from: '',
-        to: '',
-        category: ''
+
+
+    constructor(props) {
+        super(props);
+
+        const { location, from, to, category, guests } = this.props.info;
+
+        // this.state = {
+        //     locationOptions: [],
+        //     location: 'anywhere in Lebanon',
+        //     from: '',
+        //     to: '',
+        //     category: 'Category',
+        //     guests: 1
+        // }
+
+
+        this.state = {
+            locationOptions: [],
+            location: {
+                text: location === '*' ? 'Anywhere' : location,
+                value: location
+            },
+            from: from,
+            to: to,
+            category: category,
+            guests: guests || 1
+        }
+
+        console.log('constructor')
+        console.log(this.state)
     }
 
     render() {
+        // const { location, from, to, category, guests } = this.props;
         return (
-            <Menu fixed vertical>
-                <Menu.Item>
-                    <h3>Filters</h3>
-                </Menu.Item>
-                <Menu.Item>
-                    <Dropdown item text='Category'>
-                        <Dropdown.Menu>
-                            <Dropdown.Header>Category</Dropdown.Header>
-                            <Dropdown.Item>Beaches</Dropdown.Item>
-                            <Dropdown.Item>Mountains</Dropdown.Item>
-                            <Dropdown.Item>Bungalows</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Menu.Item>
-                <Menu.Item>
-                    <DateInput
-                        style={{ width: '170px' }}
-                        popupPosition='right center'
-                        name='from'
-                        placeholder='from'
-                        iconPosition='left'
-                        value={this.state.from}
-                    />
-                </Menu.Item>
-                <Menu.Item>
-                    <DateInput
-                        style={{ width: '170px' }}
-                        popupPosition='right center'
-                        name='to'
-                        placeholder='to'
-                        iconPosition='left'
-                        value={this.state.to}
-                    />
-                </Menu.Item>
-                <Menu.Item>
-                    <Grid>
-                        <Grid.Row columns={1} textAlign='center'>
-                            <Select
-                                placeholder='Anywher'
-                                options={this.state.locationOptions}
-                                value={this.state.location}
-                            />
-                        </Grid.Row>
-                    </Grid>
-                </Menu.Item>
+            <Menu vertical>
+                <Form onSubmit={this.search}>
+                    <Menu.Item>
+                        <Grid>
+                            <Grid.Row columns='2'>
+                                <Grid.Column>
+                                    <h3>Filters</h3>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Button color='red'>Click</Button>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Dropdown item text={this.state.category || 'Category'}>
+                            <Dropdown.Menu>
+                                <Dropdown.Header>Category</Dropdown.Header>
+                                <Dropdown.Item name='Beaches' onClick={this.setCategory} active={this.state.category === 'Beaches'} >Beaches</Dropdown.Item>
+                                <Dropdown.Item name='Mountains' onClick={this.setCategory} active={this.state.category === 'Mountains'}>Mountains</Dropdown.Item>
+                                <Dropdown.Item name='Bungalows' onClick={this.setCategory} active={this.state.category === 'Bungalows'}>Bungalows</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <DateInput
+                            style={{ width: '170px' }}
+                            popupPosition='right center'
+                            name='from'
+                            placeholder='from'
+                            iconPosition='left'
+                            value={this.state.from || ''}
+                            onChange={this.handleDateChange}
+                        />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <DateInput
+                            style={{ width: '170px' }}
+                            popupPosition='right center'
+                            name='to'
+                            placeholder='to'
+                            iconPosition='left'
+                            value={this.state.to || ''}
+                            onChange={this.handleDateChange}
+                        />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <h5>Guests</h5>
+                        <NumericInput
+                            name='guests'
+                            min={1}
+                            value={this.state.guests}
+                            onChange={this.handleGuests}
+                        />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Grid>
+                            <Grid.Row columns={1} textAlign='center'>
+                                <Dropdown
+                                    name="location"
+                                    value={this.state.location.value}
+                                    fluid
+                                    search
+                                    selection
+                                    text={this.state.location.text}
+                                    options={this.state.locationOptions}
+                                    placeholder='Select location'
+                                    onChange={(event, data) => this.setState({ location: { text: data.value, value: data.value } })}
+                                />
+                            </Grid.Row>
+                        </Grid>
+                    </Menu.Item>
+                </Form>
             </Menu>
         )
     }
 
     async componentDidMount() {
-        /**
-         * TODO:
-         * get locationOptions from fetch
-         * get queryString
-         * 
-         */
+        console.log('in the component')
+        console.log(this.state)
+
         const resort = new Resort();
         const locationOptions = await resort.getLocations();
+
         this.setState({ locationOptions });
+    }
+
+    handleGuests = (event) => {
+        console.log("handling" + event);
+        this.setState({ guests: event });
+    }
+
+    setCategory = (event, { name }) => {
+        this.setState({ category: name })
+    }
+
+    handleDateChange = (event, { name, value }) => {
+        if (this.state.hasOwnProperty(name)) {
+            this.setState({ [name]: value });
+        }
+    }
+
+    search = () => {
+        console.log(this.state);
+        this.props.filter();
     }
 }
 export default Filter;
--- App/cocoon-resort/src/views/explore.js
+++ App/cocoon-resort/src/views/explore.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import NavigationBar from '../components/NavigationBar';
 import Filter from '../components/Filter';
 
-class explore extends Component {
+class Explore extends Component {
 
     render() {
+        const queryString = require('query-string');
+        const info = queryString.parse(this.props.location.search)
+        console.log(info)
         return (
             <div>
                 <NavigationBar />
-                <br/>
-                <br/>
-                <br/>
-                <Filter />
+                <br />
+                <br />
+                <br />
+                <Filter info={info} filter={this.filterPackages} />
             </div>
         )
     }
+
+    filterPackages() {
+        console.log("in the parents");
+
+    }
 }
 
-export default explore;
+export default Explore;
