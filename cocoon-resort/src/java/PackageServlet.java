fa66e6e4744b35f43ef988e3771eca0f9c83ff89
rbtkay <robert.g.khayat@gmail.com>
2019-05-26 22:58:40 +0300

    starting images... big merge

--- cocoon-resort/src/java/PackageServlet.java
+++ cocoon-resort/src/java/PackageServlet.java
@@ -153,7 +153,6 @@ public class PackageServlet extends HttpServlet {
                 break;
             }
             case "updatePackage": {
-
                 String id = request.getParameter("id");
                 String token = request.getParameter("token");
 
